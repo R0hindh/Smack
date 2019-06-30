c24d6529d37c2227ca6f23f198f67099c4567f17
rohindh <rohindhrock2411@gmail.com>
2019-06-30 19:38:20 +0530

    lesson_02

---
+++ app/src/main/java/com/rohindh/smack/CreateUserActivity.kt
@@ -0,0 +1,24 @@
+package com.rohindh.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+
+    }
+    fun GenerateUserAvatarImageClick(view: View){
+
+    }
+    fun backgroundColorBtnClicked(view: View){
+
+    }
+    fun createUserBtnClicked(view: View){
+
+    }
+
+}
